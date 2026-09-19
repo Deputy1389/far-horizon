60ce335da802a536a18ec52ede662cd24f3c7c5e
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-19 16:11:12 -0700

    Load SWG DDS textures through browser-compatible loader

--- src/materials.js
+++ src/materials.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.180.0/build/three.module.js';
-import { DDSLoader } from 'https://unpkg.com/three@0.180.0/examples/jsm/loaders/DDSLoader.js';
+import { DDSLoader } from 'https://esm.sh/three@0.180.0/examples/jsm/loaders/DDSLoader.js';
 
 function seeded(seed=1){
   let s=seed>>>0;
