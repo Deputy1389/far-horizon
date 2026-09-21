def9305118e7fb29a38d3c03596fa7b48e578705
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-21 00:36:39 -0700

    world: add deterministic desert frontier city

---
+++ Source/FarHorizon/FHFrontierWorld.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FHFrontierWorld.generated.h"
+
+class UHierarchicalInstancedStaticMeshComponent;
+class UMaterialInterface;
+class UStaticMesh;
+
+UCLASS()
+class FARHORIZON_API AFHFrontierWorld : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AFHFrontierWorld();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    void BuildEnvironment();
+    void BuildCity();
+    void BuildStarport();
+    void BuildOutskirts();
+    void BuildLighting();
+    void BuildNavigation();
+    void SpawnCombatPopulation();
+
+    void AddBox(
+        UHierarchicalInstancedStaticMeshComponent* Component,
+        const FVector& Location,
+        const FVector& SizeCm,
+        const FRotator& Rotation = FRotator::ZeroRotator);
+
+    void AddCylinder(
+        UHierarchicalInstancedStaticMeshComponent* Component,
+        const FVector& Location,
+        const FVector& SizeCm,
+        const FRotator& Rotation = FRotator::ZeroRotator);
+
+    UMaterialInterface* LoadLocalMaterial(const TCHAR* AssetPath) const;
+
+    UPROPERTY()
+    TObjectPtr<UStaticMesh> CubeMesh;
+
+    UPROPERTY()
+    TObjectPtr<UStaticMesh> CylinderMesh;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> SandInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> RoadInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> WallInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> CapitalWallInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> ConcreteInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> MetalInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> LandingPadInstances;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UHierarchicalInstancedStaticMeshComponent> AntennaInstances;
+
+    FRandomStream RandomStream;
+
+    static constexpr float CityRadius = 9000.0f;
+    static constexpr float WorldRadius = 22000.0f;
+};
