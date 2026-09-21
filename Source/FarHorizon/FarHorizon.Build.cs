19661f13be6f164d712292f69e6f7beec6d67744
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-20 20:49:05 -0700

    unreal: add runtime module

---
+++ Source/FarHorizon/FarHorizon.Build.cs
@@ -0,0 +1,24 @@
+using UnrealBuildTool;
+
+public class FarHorizon : ModuleRules
+{
+    public FarHorizon(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "NavigationSystem",
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks",
+            "PhysicsCore"
+        });
+    }
+}
