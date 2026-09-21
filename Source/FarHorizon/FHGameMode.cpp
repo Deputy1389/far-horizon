8f56438497d76ab2c2538954773b6e2378cf48ea
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-20 23:42:39 -0700

    playtest: make game mode spawn foundation harness

--- Source/FarHorizon/FHGameMode.cpp
+++ Source/FarHorizon/FHGameMode.cpp
@@ -1,7 +1,66 @@
 #include "FHGameMode.h"
+
+#include "FHFoundationArena.h"
+#include "FHHUD.h"
 #include "FHPlayerCharacter.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerStart.h"
+#include "HAL/IConsoleManager.h"
+#include "Misc/CommandLine.h"
+#include "Misc/Parse.h"
 
 AFHGameMode::AFHGameMode()
 {
     DefaultPawnClass = AFHPlayerCharacter::StaticClass();
+    HUDClass = AFHHUD::StaticClass();
+}
+
+void AFHGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (FParse::Param(FCommandLine::Get(), TEXT("NoFoundationArena")))
+    {
+        return;
+    }
+
+    FActorSpawnParameters Params;
+    Params.SpawnCollisionHandlingOverride =
+        ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    GetWorld()->SpawnActor<AFHFoundationArena>(
+        FVector::ZeroVector,
+        FRotator::ZeroRotator,
+        Params);
+}
+
+AActor* AFHGameMode::ChoosePlayerStart_Implementation(
+    AController* Player)
+{
+    if (RuntimePlayerStart)
+    {
+        return RuntimePlayerStart;
+    }
+
+    if (AActor* Existing = Super::ChoosePlayerStart_Implementation(Player))
+    {
+        return Existing;
+    }
+
+    UWorld* World = GetWorld();
+    if (!World)
+    {
+        return nullptr;
+    }
+
+    FActorSpawnParameters Params;
+    Params.SpawnCollisionHandlingOverride =
+        ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    RuntimePlayerStart = World->SpawnActor<APlayerStart>(
+        FVector(-1500.0, 0.0, 110.0),
+        FRotator::ZeroRotator,
+        Params);
+
+    return RuntimePlayerStart;
 }
