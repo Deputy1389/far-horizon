ec90789e9f520b8e54ff979503c58b9e5f9356c2
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-21 00:32:39 -0700

    player: prefer migrated Epic Shooter foundation when available

--- Source/FarHorizon/FHGameMode.h
+++ Source/FarHorizon/FHGameMode.h
@@ -14,12 +14,27 @@ class FARHORIZON_API AFHGameMode : public AGameModeBase
 public:
     AFHGameMode();
 
+    virtual void InitGame(
+        const FString& MapName,
+        const FString& Options,
+        FString& ErrorMessage) override;
+
     virtual void BeginPlay() override;
 
     virtual AActor* ChoosePlayerStart_Implementation(
         AController* Player) override;
 
+    UFUNCTION(BlueprintPure, Category="Far Horizon|Foundation")
+    bool IsUsingEpicShooterFoundation() const
+    {
+        return bUsingEpicShooterFoundation;
+    }
+
 private:
+    bool TryEnableEpicShooterFoundation();
+
     UPROPERTY(Transient)
     TObjectPtr<APlayerStart> RuntimePlayerStart;
+
+    bool bUsingEpicShooterFoundation = false;
 };
