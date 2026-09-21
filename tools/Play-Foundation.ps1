7bfd194954a18e384c07ea1b1d88b20cf70fab4b
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-20 20:41:19 -0700

    tooling: require the first-person SWG hands rig

--- tools/Play-Foundation.ps1
+++ tools/Play-Foundation.ps1
@@ -68,7 +68,9 @@ if (-not $needsAssetRefresh) {
             -not $manifestData.weapons.blasterRifle -or
             -not $manifestData.characters -or
             -not $manifestData.characters.stormtrooper -or
-            [int]$manifestData.characters.stormtrooper.pipelineRevision -lt 4
+            -not $manifestData.characters.firstPersonHands -or
+            [int]$manifestData.characters.stormtrooper.pipelineRevision -lt 4 -or
+            [int]$manifestData.characters.firstPersonHands.pipelineRevision -lt 4
         )
     } catch {
         $needsAssetRefresh = $true
