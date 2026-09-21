da1764122745c492f3d5a096eaa380143f7ab636
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-20 22:29:29 -0700

    build: use UE 5.8 target defaults

--- Source/FarHorizonEditor.Target.cs
+++ Source/FarHorizonEditor.Target.cs
@@ -5,7 +5,7 @@ public class FarHorizonEditorTarget : TargetRules
     public FarHorizonEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V6;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("FarHorizon");
     }
