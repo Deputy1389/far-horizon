d8723b4c938df05811c7ad000d3d34d0fbf1d1ce
Patrick Gallagher <paddytobagen@gmail.com>
2026-09-20 20:49:12 -0700

    unreal: add runtime module

---
+++ Source/FarHorizon/FHGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FHGameMode.generated.h"
+
+UCLASS()
+class FARHORIZON_API AFHGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AFHGameMode();
+};
